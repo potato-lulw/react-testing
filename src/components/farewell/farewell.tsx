d8db5d173e5988fe2df3e4254db23523ba9235b7
potato-lulw <om.patil17072@gmail.com>
2025-03-06 02:11:13 +0530

    All about test coverages

--- src/components/farewell/farewell.tsx
+++ src/components/farewell/farewell.tsx
@@ -1,12 +1,10 @@
 //! Phase 2: Write appropriate code to solve the red errors in phase 1. This method is called Test driven development. 
 
-interface FarewellProps {
-    name?: string
-}
+import { FarewellProps } from "./farewell.types"
 
 const Farewell = (props: FarewellProps) => {
   return (
-    <div>Bye {props.name}</div> 
+    <div>Bye {props.name ? props.name : 'Guest'}</div> 
   )
 }
 
